2a97013e1b03b7855194eac6aebc6744995c5dfd
Scott Halgrim <shalgrim@gmail.com>
2020-06-14 21:13:59 -0700

    send mms

--- send_message_and_return_sid.py
+++ send_message_and_return_sid.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     # token = AccessToken(TWILIO_ACCOUNT_SID, TWILIO_API_KEY, TWILIO_API_SECRET)
     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     message = client.messages.create(
-        from_='+18588798141', body='this is a test', to='+12065791501'
+        from_='+18588798141', body='this is a test', to='+12065791501',
+        media_url = 'https://demo.twilio.com/owl.png',
     )
     print(message.sid)
